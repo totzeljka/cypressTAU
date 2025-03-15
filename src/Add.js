dcc53cf34288e93caef2b88b632452ef859e4f36
Tot Željka <120595782+totzeljka@users.noreply.github.com>
2025-03-15 19:36:04 +0100

    Add files via upload

---
+++ src/Add.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function Add() {
+    const [data, setData] = useState({
+        add_todo: ''
+    })
+    const onChange = (evt) => {
+        const key = evt.target.name;
+        const value = evt.target.value;
+        setData(oldData => ({ ...oldData, [key]: value }))
+    }
+    return (
+        <input data-testid="todo-input" value={data.add_todo} onChange={onChange} name="add_todo" />
+    );
+}
---
+++ src/applitools.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    testConcurrency: 1,
+    apiKey: 'AQY6oL0iHIcZKRiCT0WYpwijRrRBk4W98XvBpEg3Yxos110',
+    batchName: 'TAU Todo App',
+    browser: [
+        { width: 800, height: 600, name: 'chrome' },
+        { deviceName: 'Pixel 2', screenOrientation: 'portrait' }
+    ]
+}
